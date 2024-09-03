785e57505e1533b51b281be3cbf4f05609b3d1f5
Annie Wang <annie.niwang@hotmail.com>
2024-09-03 20:29:04 +1000

    Added cards, request,highline routers

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,17 @@
 const express = require('express');
 const cors = require('cors');
 const router =  require ('./routes/index');
+const {handleErrors} = require('./middleware/errorMiddleware')
 //create web server
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+
 app.use('/api',router)
+//use error middleware at the end
+app.use(handleErrors)
 
 const PORT = 80;
 app.listen(PORT, function(){
